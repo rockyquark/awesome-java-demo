788c3c80eac5c292253a66ff6406e762b17d8883
肖锦光 <marchawake@gmail.com>
2023-11-21 21:44:58 +0800

    Java 测试实践例子项目

---
+++ src/main/java/org/keep/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package org.keep.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 肖锦光
+ * @version 0.1.0
+ * @note Java 实践案例项目 SpringBoot 入口
+ * @date 2023/10/18 22:21
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
